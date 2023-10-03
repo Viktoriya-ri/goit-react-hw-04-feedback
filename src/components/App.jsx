e7b9d76a72196ad27f0ab2bae7c034064ba4e684
Вікторія <viktoriya23071@gmail.com>
2023-10-03 21:36:47 +0300

    s

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,54 @@
-import React from 'react';
+import { useState } from 'react';
 
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
-import IsEmptyNotification from './Notification/Notification'
-import {Wrapper} from './App.styled';
+import IsEmptyNotification from './Notification/Notification';
+import { Wrapper } from './App.styled';
 
-class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleClick = option => {
+    switch (option) {
+      case 'good':
+        setGood(s => s+1);
+        break;
+
+      case 'neutral':
+        setNeutral(s => s+1);
+        break;
+
+      case 'bad':
+        setBad(s => s+1);
+        break;
+      default:
+        throw new Error('Invalid option');
+    }
   };
 
-
-  handleClick = (option) => {
-
-    this.setState(
-      prevState => ({
-        [option]: prevState[option] + 1,
-      })
-    );
-  };
-
-  render() {
-
-     const {good,neutral,bad} = this.state;
-     const total = good+neutral+bad;
-     const positivePercentage = (good/total)*100;
-    return (
-      <Wrapper>
-        <Section>
-          <FeedbackOptions
-            onClick={this.handleClick}
-            options={Object.keys(this.state)}
-        
-          />
-             <Statistics
-         good={good}
-         neutral={neutral}
-         bad={bad}
-         total={total}
-         positivePercentage={positivePercentage}
-       /> {total===0 && <IsEmptyNotification/>}
-        </Section>
-      </Wrapper>
-    );
-  }
-}
+  const total = good + neutral + bad;
+  const positivePercentage = (good / total) * 100;
+  return (
+    <Wrapper>
+      <Section>
+        <FeedbackOptions
+          onClick={handleClick}
+          options={Object.keys({good,neutral,bad})} // Разобрать
+        />
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positivePercentage={positivePercentage}
+        />{' '}
+        {total === 0 && <IsEmptyNotification />}
+      </Section>
+    </Wrapper>
+  );
+};
 
 export default App;
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
-
 import { MainSection } from './Section.styled';
 
-class Section extends React.Component {
-  render() {
+const Section = ({children}) =>  {
+
     return (
       <MainSection>
         <h1>Please leave a feedback</h1>
-        {this.props.children}
+        {children}
       </MainSection>
     );
-  }
 }
 
 export default Section;
